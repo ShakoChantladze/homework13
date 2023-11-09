07de6f5f304a8801e085eeea04e83f8136005447
ShakoChantladze <shako.chantladze@gmail.com>
2023-11-09 15:00:15 +0400

    create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+let userName="Shako";
+console.log(userName)
+let age=23;
+console.log(age)
+let isMarried=false;
+console.log(isMarried)
+
+
+
+var firstName = "Shako";
+var lastName = "Chantladze";
+
+var fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+var person={
+    firstName:"Shako",
+    lastName:"Chantladze",
+    age:23,
+}
+console.log("I'm" + " " + person.firstName)
+
+var a = 23;
+var b = 7;
+var c = 44;
+var l=15;
+var m=75;
+
+var sum = a + b + c;
+
+console.log(sum);
+
+var deduction=c-l
+
+console.log(deduction);
+
+var multiplication=a*l
+
+console.log(multiplication)
+
+var division=m/l
+
+console.log(division)
+
+var length=7;
+var width=5;
+
+var area=length*width
+
+console.log(area)
